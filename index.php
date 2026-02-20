ce68653a4029cf79a2c64d48744a7f5d2895cbe5
Juangomez0122 <juandejesusgomezsanchez12@gmail.com>
2026-02-19 19:44:22 -0600

    se crea la estructura inicial del archivo index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistemon</title>
+</head>
+<body>
+    <h1>Bien vemidos al proyecto de</h1>
+    <br>
+    <img src="./equipo.jpg" alt="Imagen del equipo" width="150px" height="150px">
+    <br>
+    <h2>Integrantes</h2>
+    <ul>
+        <li><a href="./angel.dock">Descarga el cv de Ángel cruz</a></li>
+        <li><a href="./saul.dock">Descarga el cv de Saul de la cruz</a></li>
+        <li><a href="./jehovani.dock">Descarga el cv de Jehovani de la cruz</a></li>
+        <li><a href="./cesar.dock">Descarga el cv de Cesar de la cruz</a></li>
+        <li><a href="./juan.dock">Descarga el cv de Juan de la cruz</a></li>
+    </ul>
+    
+    <link rel="stylesheet" href="Descargar el link de descarga del cv">
+</body>
+</html>
